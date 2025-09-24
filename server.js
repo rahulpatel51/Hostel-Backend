73bbc73f54418398177a2e7bc63ecb5d7df0b048
Rahul Patel <ra.rahul11131@gmail.com>
2025-09-25 04:24:55 +0530

    new change

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(
   cors({
     origin: [
       process.env.CLIENT_URL || "http://localhost:3000", 
-      "http://localhost:3001", 
+      "http://localhost:3001" || "https://hostel-admin-frontend.vercel.app", 
     ],
     credentials: true,
   })
